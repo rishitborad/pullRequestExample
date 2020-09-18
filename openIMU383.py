ff9a63097772d6391498377e42035e75a8195315
rishitborad <rishitborad@gmail.com>
2020-09-17 21:42:46 -0700

    cleanups and path change

--- openIMU383.py
+++ openIMU383.py
@@ -8,28 +8,24 @@ Created on 2020-02-14
 import serial
 import time
 import datetime
-#from azureSAStest import push2azure
 import glob
 import random
 
 ser = serial.Serial('/dev/cu.usbserial-FT8YAYV7', 921600, parity='N',bytesize=8,stopbits=1,timeout=None) # need to confirm port
-
-
 while True:
     if ser.isOpen(): break
 
 print ('\Port is open now\n')
-#configFRII(ser)
 ser.flushInput()
 
 random_num = str(random.randint(100,10000))
 
-fname = './data/OpenIMU385-USB1_'
-fname += random_num
-fname += time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()) + '.bin'
-fmode = 'wb'
+name = './OpenIMU_'
+name += random_num
+name += time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime()) + '.bin'
+mode = 'wb'
 
-with open(fname,fmode) as outf:
+with open(name,fmode) as outf:
     while True:
         try:
             line = ser.readline()
